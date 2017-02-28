97e1578ca4def089a392d1f0f5d69346b5e087c4
liuxiwen <xiwenhehe@sina.com>
2017-02-28 10:38:54 +0800

    传统线程技术中的定时器技术

--- src/main/java/concurrency/ch2/TraditionalTimer01.java
+++ src/main/java/concurrency/ch2/TraditionalTimer01.java
@@ -7,7 +7,7 @@ import java.util.TimerTask;
 /**
  * Created by liuxiwen on 2017/2/27.
  */
-public class TraditionalTimer {
+public class TraditionalTimer01 {
 
     public static void main(String[] args) {
 
